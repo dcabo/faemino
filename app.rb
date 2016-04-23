1f3fa852139a908ddbc7d114591e84233fe08bae
David Cabo <david.cabo@gmail.com>
2016-04-23 14:44:03 +0200

    Fix JSON dependencies

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require 'sinatra'
 require 'sinatra/json'
 
 get '/' do
-  json {
+  json({
     response_type: "in_channel",
     text: "El budista https://www.youtube.com/watch?v=K_wJsX8FQVg"
-  }
+  })
 end
