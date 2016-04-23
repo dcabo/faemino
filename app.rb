54434633d8894cceb87649c28796547317710449
David Cabo <david.cabo@gmail.com>
2016-04-23 14:40:36 +0200

    Fix JSON encoding

--- app.rb
+++ app.rb
@@ -1,8 +1,9 @@
 require 'sinatra'
+require 'sinatra/json'
 
 get '/' do
-  {
-      "response_type": "in_channel",
-      "text": "El budista https://www.youtube.com/watch?v=K_wJsX8FQVg"
+  json {
+    response_type: "in_channel",
+    text: "El budista https://www.youtube.com/watch?v=K_wJsX8FQVg"
   }
 end
