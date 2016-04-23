4780ce7c6bc4acf84ff3589b05a5701839bfa9d0
David Cabo <david.cabo@gmail.com>
2016-04-23 15:18:50 +0200

    Add YouTube API integration

--- app.rb
+++ app.rb
@@ -1,9 +1,42 @@
 require 'sinatra'
 require 'sinatra/json'
+require 'google/api_client'
+
+require './env' if File.exists?('env.rb')
+
+YOUTUBE_API_SERVICE_NAME = 'youtube'
+YOUTUBE_API_VERSION = 'v3'
+
+# See https://developers.google.com/youtube/v3/docs/search/list#examples
+def get_service
+  client = Google::APIClient.new(
+    :key => ENV['GOOGLE_API_KEY'],
+    :authorization => nil,
+    :application_name => $PROGRAM_NAME,
+    :application_version => '1.0.0'
+  )
+  youtube = client.discovered_api(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION)
+
+  return client, youtube
+end
+
 
 get '/' do
+  client, youtube = get_service
+
+  search_response = client.execute!(
+    :api_method => youtube.search.list,
+    :parameters => {
+      :part => 'snippet',
+      :q => 'Faemino Manuel Campo Vidal',
+      :type => 'video',
+      :maxResults => 1
+    }
+  )
+
   json({
     response_type: "in_channel",
-    text: "El budista https://www.youtube.com/watch?v=K_wJsX8FQVg"
+    text: "#{search_response.data.items[0].snippet.title} " +
+          "https://www.youtube.com/watch?v=#{search_response.data.items[0].id.videoId}"
   })
 end
